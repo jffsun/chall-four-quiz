33d34b329f2dafc1539ca6aff344dfe692b55253
Jeffrey Sun <jeffreysun811@gmail.comgit configgit config --global user.name Jeffrey>
2022-08-04 18:03:29 -0700

    images, readme, styling

---
+++ README.md
@@ -0,0 +1,40 @@
+# Challenge Four: Javascript Quiz
+
+## Project Description
+
+Challenge four's requirement was to built a fully functioning quiz web application with Javascript's Web APIs. This application functions correctly if the user is given a timed quiz which saves their score afterwards. The user should then be able to compare their score with past high scores. The timed quiz on Javascript fundamentals informs the user whether their selected answer for each question is correct or not and subtracts 5 seconds to the timer for each missed question.
+
+### Application Screenshot
+
+The following images shows the quiz web application:
+
+![Application](./assets/images/screenshot1.jpg)
+
+### Begin the Quiz
+
+The following image shows the first question and its answer options. The start button is disabled and the timer has begun.
+
+![Start Quiz](./assets/images/screenshot2.jpg)
+
+### Answer Check
+
+The application informs the user whether their selected answer is correct or incorrect.
+
+![Correct Answer](./assets/images/screenshot3.jpg)
+![Incorrect](./assets/images/screenshot4.jpg)
+
+### Saving Your Score
+
+After taking the quiz, the user will store their score and their intials into local storage.
+![Ask For Initials](./assets/images/screenshot5.jpg)
+
+### High Scores
+
+After entering your initials and saving your score, you are able to view the top 3 scores from previous quiz attempts. 
+![High Scores](./assets/images/screenshot6.jpg)
+
+### Link to Github Page
+[https://jffsun.github.io/chall-four-quiz/](https://jffsun.github.io/chall-four-quiz/).
+
+### Link to Github Respository
+[https://github.com/jffsun/chall-four-quiz](https://github.com/jffsun/chall-four-quiz).
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,116 +1,121 @@
 var startButton = document.querySelector(".start-button");
 var timerElement = document.querySelector(".timer-count");
-var question = document.querySelector(".question");
-var questionNumber = document.querySelector(".question-number");
 var questionPanel = document.querySelector(".question-panel")
+var answerChoices = document.querySelector(".answer-buttons");
+var question = document.querySelector(".question");
 var optionA = document.getElementById("optionA");
 var optionB = document.getElementById("optionB");
 var optionC = document.getElementById("optionC");
 var optionD = document.getElementById("optionD");
-var form = document.querySelector("form");
-var submit = document.getElementById("submit")
-var answerChoices = document.querySelector(".btn-group-vertical");
-var quizDone = false;
+var result = document.getElementById("result");
 var timer;
 var timerCount;
 var questionsCorrect = 0;
 var questionNumber;
-var verticalButtons = document.querySelector(".btn-group-vertical")
-var scoreList = document.querySelector(".score-list")
+var initials;
 
-// Questions as objects and their answers
+// Questions as objects and their answer options
 var Questions = [{
-    q: "What does '.js' mean at the end of the file",
-    A: "1A",
-    B: "1B",
-    C: "1C Javascript",
-    D: "1D",
-    answer: "1C Javascript",
+    q: "1. Which of the following are used when naming a Javascript file?",
+    A: ".md",
+    B: ".css",
+    C: ".js",
+    D: ".html",
+    answer: ".js",
 }, 
 {
-    q: "Question 2",
-    A: "2A",
-    B: "2B Answer",
-    C: "2C",
-    D: "2D",
-    answer: "2B Answer",
+    q: "2. Which of the following tags should we put our Javascript in?",
+    A: "javascript",
+    B: "script",
+    C: "js",
+    D: "scripting",
+    answer: "script",
 },
 {
-    q: "Question 3",
-    A: "3A",
-    B: "3B Answer",
-    C: "3C",
-    D: "3D",
-    answer: "3B Answer",
+    q: "3. How would we write 'Hello World' in a window alert?",
+    A: "window.alert('Hello World')",
+    B: "window.prompt('Hello World')",
+    C: "window.confirm('Hello World')",
+    D: "window.open('Hello World')",
+    answer: "window.alert('Hello World')",
 },
 {
-    q: "Question 4",
-    A: "4A",
-    B: "4B",
-    C: "4C",
-    D: "4D Answer",
-    answer: "4D Answer",
+    q: "4. How do you create a function in Javascript?",
+    A: "function: myFunction()",
+    B: "function = myFunction()",
+    C: "myFunction(): function",
+    D: "function myFunction()",
+    answer: "function myFunction()",
 },
 {
-    q: "Question 5",
-    A: "5A Answer",
-    B: "5B",
-    C: "5C",
-    D: "5D",
-    answer: "5A Answer",
+    q: "5. What do x and y represent in: myFunction(x, y)?",
+    A: "function values",
+    B: "function parameters",
+    C: "input",
+    D: "function variables",
+    answer: "function parameters",
+},
+{
+    q: "6. How does a 'loop' start?",
+    A: "for (i<=5; i++)",
+    B: "for (i=0; i<=5)",
+    C: "for (i=0; i<=5; i++)",
+    D: "for i=1 to 5",
+    answer: "for (i=0; i<=5; i++)",
+},
+{
+    q: "7. How do you add a comment in Javascript?",
+    A: '"This is a comment"',
+    B: "`This is a comment`",
+    C: "#This is a comment",
+    D: "// This is a comment",
+    answer: "// This is a comment",
+},
+{
+    q: "8. In Javascript, the expression x != y returns false if:",
+    A: "the variables are equal",
+    B: "x is less than y",
+    C: "the variables are not equal",
+    D: "none of the above",
+    answer: "the variables are equal",
+},
+{
+    q: "9. A named element in a JavaScript program that is used to store and retrieve data is a:",
+    A: "variable",
+    B: "string",
+    C: "method",
+    D: "function",
+    answer: "variable",
+},
+{
+    q: "10. In a form, if you want users to select only one option out of many, use:",
+    A: "text boxes",
+    B: "check boxes",
+    C: "both text boxes and check boxes",
+    D: "radio buttons",
+    answer: "radio buttons",
 },
 ]
-
-function init() {
-    getScores();
-}
-
-init();
-
-function getScores() {
-    // Get stored data from local storage and parse into array elements
-    var storedQuizProfiles = JSON.parse(localStorage.getItem("quizProfile"));
-    
-    // If there are quizProfiles to retrieve, then update scores array
-    if (storedQuizProfiles !== null) {
-        scores = storedQuizProfiles;
-        
-    } else {
-        scores = [];
-        return;
-    }
-
-}
-
-function renderScores() {
-    todoList.innerHTML = "";
-
-  // Add text to #todoCount. Displays the length of the todo array
-  todoCountSpan.textContent = todos.length;
-}
-
 // Click start to begin quiz
 startButton.addEventListener("click", startGame);
 
-// The startGame function is called when the start button is clicked
+// Game starts when start button is clicked
 function startGame() {
-    timerCount = 60;
 
     // Prevents start button from being clicked when quiz is in progress
     startButton.disabled = true;
+
+    timerCount = 60;
     startTimer();
-    console.log("game start");
     questionNumber = 0;
     
         // Generate first question
         generateQuestion(questionNumber);
-        console.log("question displayed");
 
-        // Generate first question's answers
+        // Generate answer options for first question
         generateAnswers(questionNumber);
-        console.log("answer options displayed");
 
-        // Click answer button validates that answer
+        // Clicking an answer choice validates that answer
         answerChoices.addEventListener("click", function(event) {
             var selected = event.target;
             
@@ -120,14 +125,14 @@ function startGame() {
                 // Check selected answer against question answer
                 validateAnswer(selected);
 
-                // Generate new question if length of questions not reached
+                // Generate new question if there are more questions left
                 if (questionNumber < Questions.length) {
                     generateQuestion(questionNumber);
                     generateAnswers(questionNumber);
 
                     } else {
 
-                        // Show end screen after last question answered
+                        // Show end screen if last question answered
                         scoreScreen();
                     }
             }
@@ -142,7 +147,7 @@ function generateQuestion(questionNumber) {
 
 function generateAnswers(questionNumber) {
 
-    // Inject option choices at questionNumber to each answer button
+    // Inject answer option text into each option
     optionA.innerHTML = Questions[questionNumber].A;
     optionB.innerHTML = Questions[questionNumber].B;
     optionC.textContent = Questions[questionNumber].C;
@@ -151,14 +156,22 @@ function generateAnswers(questionNumber) {
 
 function validateAnswer (selected) {
     var answer = Questions[questionNumber].answer;
-    if (selected.textContent == answer) {
-        console.log("correct");
-        questionsCorrect++;
-        questionNumber++;
-    } else {
-        console.log("incorrect");
 
-        // Lose 5 seconds if answer is incorrect
+    // If user's answer is correct
+    if (selected.textContent == answer) {
+        result.innerHTML = "Correct! ✅";
+
+        // Increment score
+        questionsCorrect++;
+
+        // Move onto next question
+        questionNumber++;
+
+        // If incorrect
+    } else {
+        result.innerHTML = "Incorrect. ❌";
+
+        // Subtract 5 seconds from time
         timerCount -= 5;
         questionNumber++;
     }
@@ -166,51 +179,87 @@ function validateAnswer (selected) {
 
 // Counts timer down from 60
 function startTimer() {
+    
     timer = setInterval(function() {
         timerCount--;
 
-        // Show count text
+        // Show seconds remaining
         timerElement.textContent = timerCount;
 
         // If timer runs out before all questions answered
-        if (timerCount === 0) {
+        if (timerCount < 0) {
+            clearInterval(timer)
             scoreScreen();
         }
     }, 1000);
 }
 
 function scoreScreen () {
+
+    // Stop timer
     clearInterval(timer);
+    timerCount = 0;
+
+    // Alert user their quiz score
     window.alert(`You scored ${questionsCorrect} out of 10!`);
 
     // Ask for user initials and only accept two characters
     var initials = ""
     while (initials.length !== 2) {
         initials = window.prompt("Enter your initials below. Initials must be two characters");
+        initials = initials.toUpperCase()
     }
     saveLastQuiz(questionsCorrect, initials);
 }
 
-function displayHighScores(scores) {
-    for (i=0; i < scores.length; i++) {
-        if (i > 2) {
-            break;
-        }
-        console.log(scores[i]);
-    }
-}
-
 function saveLastQuiz(questionsCorrect, initials) {
+
+    // Store user data as an object
     var quizProfile = {
         user: initials,
         score: questionsCorrect,
     };
-    scores = (JSON.parse(localStorage.getItem("scores")||'[]'));
+
+    // Scores are retrieved from local storage and scores array is updated either with previous scores from local storage or to an empty array if none
+    scores = (JSON.parse(localStorage.getItem("scores") || '[]'));
+
+    // Add the new quiz score to scores array
     scores.push(quizProfile);
+
+    // Sort array by questionsCorrect from highest to low
     scores.sort((a, b) => {
         return b.score - a.score;
     });
+
+    // Show user highest 3 quiz scores
     displayHighScores(scores);
-    // Store score and intials to local storage
+
+    // Store score and intials to local storage as string
     localStorage.setItem("scores", JSON.stringify(scores));
+}
+
+// Display max three scores 
+function displayHighScores(scores) {
+
+    // If user's quiz score is only saved score
+    if (scores.length == 1) {
+        window.alert(`Your score\n
+        ${scores[0].user} scored ${scores[0].score}`);
+
+    // If one previously saved score
+    } else if (scores.length == 2) {
+        window.alert(`Top Two Students\n 
+        1st: ${scores[0].user} scored ${scores[0].score}\n
+        2nd: ${scores[1].user} scored ${scores[1].score}\n
+        Your score: ${questionsCorrect}`);
+    } else {
+
+        // If multiple saved scores
+        window.alert(`Top Three Students\n 
+        1st: ${scores[0].user} scored ${scores[0].score} points\n
+        2nd: ${scores[1].user} scored ${scores[1].score} points\n
+        3rd: ${scores[2].user} scored ${scores[2].score} points\n
+        Your score: ${questionsCorrect}`);
+    }
+    
 }
